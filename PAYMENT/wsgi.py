a52d1e9ce72a6d904a2533439a130785690e13e5
jayanth4255 <218a1a4255@risekrishnasaiprakasam.edu.in>
2025-11-29 23:16:31 +0530

    Force collectstatic on startup in WSGI

--- PAYMENT/wsgi.py
+++ PAYMENT/wsgi.py
@@ -10,7 +10,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PAYMENT.settings')
+
+# Fail-safe: Run collectstatic on startup
+try:
+    call_command('collectstatic', interactive=False)
+except Exception as e:
+    print(f"Error collecting static files: {e}")
 
 application = get_wsgi_application()
