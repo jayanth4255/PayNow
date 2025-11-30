a8c50187c1719f379a017323d2ad82d682737787
jayanth4255 <218a1a4255@risekrishnasaiprakasam.edu.in>
2025-11-30 09:39:58 +0530

    Clean up settings and fix wsgi.py

--- PAYMENT/wsgi.py
+++ PAYMENT/wsgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
-
+from os import path
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PAYMENT.settings')
 
 application = get_wsgi_application()
